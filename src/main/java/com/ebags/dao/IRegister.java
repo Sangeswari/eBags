4370d0820df825d953e3af75643730cfc3581cde
Sangeswari <sanges333@gmail.com>
2017-04-17 11:02:14 +0530

    Add files via upload

---
+++ src/main/java/com/ebags/dao/IRegister.java
@@ -0,0 +1,11 @@
+package com.ebags.dao;
+
+import java.util.List;
+
+import com.ebags.model.Register;
+
+
+public interface IRegister {
+	public void addRegisteration(Register register);
+	public List<Register> getRegisteration();
+}
---
+++ src/main/java/com/ebags/service/IRegisterService.java
@@ -0,0 +1,11 @@
+package com.ebags.service;
+
+import java.util.List;
+
+import com.ebags.model.Register;
+ 
+public interface IRegisterService {
+
+		public void addRegistration(Register registerBean);
+		public List<Register> getRegisteration();
+}
